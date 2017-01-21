b4bf77ce2f9a07a6d306145a36b0f18ee22c3be0
aaronjaymus <aaronjaymus@gmail.com>
2017-01-21 10:32:33 -0500

    Taking in data from form fields and creating a trip object. Changed the document.ready function to just contail mapInit rather than whole page.

--- assets/js/mapCode.js
+++ assets/js/mapCode.js
@@ -1,23 +1,38 @@
-$(document).ready(function(){
 	var directionsDisplay;
-	var directionsService = new google.maps.DirectionsService();
+	var directionsService;
 	var map;
-
+	var trips = [];
 	//initializes the map and directions renderer
 	function mapInit() {
 		console.log("mapInit called");
+		directionsService = new google.maps.DirectionsService();
 		directionsDisplay = new google.maps.DirectionsRenderer({polylineOptions: {clickable: false}});
 		var mapCanvas = $("#map")[0];
 		var mapOptions = {
 		  center: new google.maps.LatLng(39.83, -98.58), 
-		  zoom: 4
+		  zoom: 3
 		}
 		map = new google.maps.Map(mapCanvas, mapOptions);
 		google.maps.event.addListener(map, 'click', function(event){
 			placeMarker(map, event.latLng);
 		});
 		directionsDisplay.setMap(map);
-	}
+	};
+
+	function buildTrip(){
+		var trip = {
+			tripName : $("#tripName").val().trim(),
+			startDate : $("#startDate").val().trim(),
+			endDate : $("#endDate").val().trim(),
+			startPoint : $("#origin").val().trim(),
+			endPoint : $("#destination").val().trim(),
+			tank : $("#tankSize").val().trim(),
+			mileage : $("#mpg").val().trim() 
+		};
+		console.log(trip);
+		trips.push(trip);
+		calcRoute(trip.startPoint, trip.endPoint);
+	};
 
 	//places marker on map. the location variable contains lat and long, easy to extract
 	function placeMarker(map, location){
@@ -34,7 +49,7 @@ $(document).ready(function(){
   		});
 
   		getGas(lat, lng);
-	}
+	};
 	//takes latitude and longitude from marker placement to get gas data in a 10 mile radius
 	function getGas (lat, lng){
 		var queryURL = "http://api.mygasfeed.com/stations/radius/"+lat+"/"+lng+"/10/reg/Price/lg2dvyvl7v.json";
@@ -61,8 +76,8 @@ $(document).ready(function(){
 
 	//simple route calculator with a default of driving. easy enough to send arguments into function to replace start and end.
 	function calcRoute(start, end) {
-		var start = "New York, NY";
-		var end = "2003 Watson Rd, Durham, NC";
+		var start = start;
+		var end = end;
 		var request = {
 			origin: start,
 			destination: end,
@@ -77,8 +92,13 @@ $(document).ready(function(){
 	}
 
 
-
+$(document).ready(function(){
 	mapInit();
-	calcRoute();
+	//calcRoute();
+});
+
+$("#submit").click(function(event){
+	event.preventDefault();
+	buildTrip();
 });
 
